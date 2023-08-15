237c7669eeeb2f29c3b414b184384d09703dc43c
Ian Almeida <iangtaonline1@gmail.com>
2023-08-15 17:14:18 -0300

    ajustando header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import logo from './logo.svg'
+import './header.css'
 
 function Header() {
     return(
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
+            <ul className='Links-header'>
+                <li><a href=""><span>Home</span></a></li>
+                <li><a href=""><span>Habilidades</span></a></li>
+                <li><a href=""><span>Sobre</span></a></li>
+                <li><a href=""><span>Portfolio</span></a></li>
+                <li><a href=""><span>Contatos</span></a></li>
+            </ul>
         </header>
     )
 }
