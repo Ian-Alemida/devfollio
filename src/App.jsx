f1d1cc726eed146a85091358706873c74b1a39bf
Ian Almeida <iangtaonline1@gmail.com>
2023-08-15 17:14:04 -0300

    mexendo no alinhamento

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
+import './reset.css'
 import Header from './Components/Header/Header';
+import Banner from './Components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Banner></Banner>
     </div>
   );
 }
