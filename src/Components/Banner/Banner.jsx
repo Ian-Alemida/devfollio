b1fa432858f269a93fc58232a0db59b88408a0c4
Ian Almeida <iangtaonline1@gmail.com>
2023-08-15 21:05:52 -0300

    add animation text

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,11 +6,20 @@ function Banner (){
     return (
         <div className='content'>
             <div className='content-text'>
-                <span>Hello word! / Olá mundo!</span>
-                <h2>Prazer, Eu sou Ian Almeida</h2>
-                <p>Sou um desenvolvedor Front-End</p>
+                <div class="content-animation">
+                    <div class="content-animation__container">    
+                        <ul class="content-animation__container__list">
+                            <li class="content-animation__container__list__item">Hello world !</li>
+                            <li class="content-animation__container__list__item">Olá mundo !</li>
+                        </ul>
+                    </div>
+                </div>
+                <h2>Prazer, Eu sou Ian Almeida.</h2>
+                <p className='animation'>Sou um desenvolvedor Front-End...</p>
             </div>
-            <div className='content-img'><img src={image} className='img' alt="" /></div>
+            <div className='content-img'>
+                <img src={image} className='img' alt="" />
+            </div>
         </div>
     )
 }
