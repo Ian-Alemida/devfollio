f70eb4d07d28dd78e6aceb3d6893eac6bca199fd
Ian Almeida <iangtaonline1@gmail.com>
2023-08-16 00:13:33 -0300

    add button

---
+++ src/Components/Banner/Botao/Botao.jsx
@@ -0,0 +1,12 @@
+import './botao.css'
+
+function Botao (){
+    return (
+        <button class="animated-button">
+            <span>Hover me</span>
+            <span></span>
+        </button>
+    )
+}
+
+export default Botao
