bc62ae79048d9a0d7a61c0901413163b5ed6d773
Ian Almeida <iangtaonline1@gmail.com>
2023-08-16 23:29:50 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import './reset.css'
 import Header from './Components/Header/Header';
 import Banner from './Components/Banner/Banner';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Banner></Banner>
+      <About></About>
     </div>
   );
 }
