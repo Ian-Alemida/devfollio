c6ce6f8a378dbc2a2d9ec67d0a39cf2e9556e604
Ian Almeida <iangtaonline1@gmail.com>
2023-08-16 22:34:57 -0300

    create section about

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,12 @@
+import './about.css'
+
+function About () {
+    return (
+        <div className="content">
+            <div className='content-img'></div>
+            <div className='conntent-text'></div>
+        </div>
+    )
+}
+
+export default About
