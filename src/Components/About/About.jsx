00170d933adb8f0311b63cd529925c77905c4dee
Ian Almeida <iangtaonline1@gmail.com>
2023-08-18 01:50:28 -0300

    finish about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -10,7 +10,7 @@ function About () {
     return (
         <div className="content-about">
             <div className='content-about-img'>
-                <Card children1={<img src={perfil} alt="" />} children2={<img src={perfil2} alt="" />}></Card>
+                <Card children1={<img src={perfil} alt="Uma foto minha" />} children2={<img src={perfil2} alt="PixelArt de um homem em seu computador" />}></Card>
             </div>
             <div className='content-about-text'>
                 <h2>Sobre mim</h2>
@@ -18,7 +18,7 @@ function About () {
                 <p>Ola! Eu sou um Desenvolvedor Front-end especializado  em React.js <br /> Possuo habilidades solidas em HTML5 CSS3 e JavaScript.</p>
                 <p>Atualmente estou  cursando Análise e Desenvolvimento de Sistemas, no primeiro semestre do curso. Entrei com o objetivo de aperfeiçoar minhas habilidades, pois estou em constante desenvolvimento.</p>
                 <p>Desde de 2022, venho atuando no desenvolvimento de software e posso afirmar que estou apaixonado por essa  área. Sempre fui fascinado  por tecnologia e tive meus primeiros contatos coom a computação ainda muito cedo.</p>
-                <div>
+                <div className='content-button'>
                     <Botao icon={iconDownload} text={'Baixar CV'}></Botao>
                     <Botao icon={instagramIcon} text={'Instagram'}></Botao>
                 </div>
