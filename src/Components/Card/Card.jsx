deb907a5f1e68a3fc58fe527aa44ceafd0ac00cc
Ian Almeida <iangtaonline1@gmail.com>
2023-08-17 21:26:39 -0300

    add card

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,16 @@
+import './card.css'
+
+function Card () {
+    return (
+        <div class="card">
+            <div class="first-content">
+                <span>First</span>
+            </div>
+            <div class="second-content">
+                <span>Second</span>
+            </div>
+        </div>
+    )
+}
+
+export default Card
