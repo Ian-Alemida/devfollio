0b54ee80140657f388d1d7cd19785aaa4d6b2803
Ian Almeida <iangtaonline1@gmail.com>
2023-08-17 22:38:10 -0300

    add button instagram

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,6 +3,7 @@ import perfil from './perfil.jpg'
 import perfil2 from './perfil2.png'
 import Botao from '../Banner/Botao/Botao'
 import iconDownload from './download-icon.svg'
+import instagramIcon from './instagram-icon.svg'
 import Card from '../Card/Card'
 
 function About () {
@@ -17,8 +18,11 @@ function About () {
                 <p>Ola! Eu sou um Desenvolvedor Front-end especializado  em React.js <br /> Possuo habilidades solidas em HTML5 CSS3 e JavaScript.</p>
                 <p>Atualmente estou  cursando Análise e Desenvolvimento de Sistemas, no primeiro semestre do curso. Entrei com o objetivo de aperfeiçoar minhas habilidades, pois estou em constante desenvolvimento.</p>
                 <p>Desde de 2022, venho atuando no desenvolvimento de software e posso afirmar que estou apaixonado por essa  área. Sempre fui fascinado  por tecnologia e tive meus primeiros contatos coom a computação ainda muito cedo.</p>
-                <div><Botao icon={iconDownload} text={'Baixar CV'}> </Botao>
-            </div></div>
+                <div>
+                    <Botao icon={iconDownload} text={'Baixar CV'}></Botao>
+                    <Botao icon={instagramIcon} text={'Instagram'}></Botao>
+                </div>
+            </div>
         </div>
     )
 }
