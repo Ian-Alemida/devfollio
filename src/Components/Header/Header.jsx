cc67bfd82459d247a3ab3f67fffee05b17327e8b
Ian Almeida <iangtaonline1@gmail.com>
2023-08-19 20:02:12 -0300

    ajust

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,8 +7,8 @@ function Header() {
             <img src={logo} className="App-logo" alt="logo" />
             <ul className='Links-header'>
                 <li><a href=""><span>Home</span></a></li>
-                <li><a href=""><span>Habilidades</span></a></li>
                 <li><a href=""><span>Sobre</span></a></li>
+                <li><a href=""><span>Habilidades</span></a></li>
                 <li><a href=""><span>Portfolio</span></a></li>
                 <li><a href=""><span>Contatos</span></a></li>
             </ul>
