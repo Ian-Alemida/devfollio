32818f82b0755d01977c42ec7bef00b2c1fea125
Ian Almeida <iangtaonline1@gmail.com>
2023-08-20 18:17:57 -0300

    refatoring

--- src/Components/Technology/CardTechnology/CardTechnology.jsx
+++ src/Components/Technology/CardTechnology/CardTechnology.jsx
@@ -1,8 +1,29 @@
 import './CardTechnology.css';
+import React, { useState } from 'react';
 
 function CardTechnology () {
-    return(
+    const [isHover, setIsHover] = useState(false)
 
+    const handleHover = () => {
+        setIsHover(true);
+        document.querySelectorAll('.technology-card').forEach(card => {
+            card.classList.add('technology-card-not-hover')
+        })
+    }
+
+    const handleMouseLeave = () => {
+        setIsHover(false);
+        document.querySelectorAll('.technology-card').forEach( card => {
+            card.classList.remove('technology-card-not-hover')
+        })
+    }
+
+    return(
+        <div 
+            className={`technology-card ${isHover ? 'technology-card-hover' : ''} `}
+            onMouseEnter={handleHover}
+            onMouseLeave={handleMouseLeave}>
+        </div>
     )
 }
 
--- src/Components/Technology/Technology.jsx
+++ src/Components/Technology/Technology.jsx
@@ -1,45 +1,11 @@
 import './technology.css';
-import React, { useState } from 'react';
 
 function Technology (){
-    const [isHover, setIsHover] = useState(false);
-    const [isAnyHover, setIsAnyHover] = useState(false);
-    
-    const handleHover = () => {
-        setIsHover(true);
-        document.querySelectorAll('.technology-card').forEach(card => {
-            card.className.add('technology-card-not-hover')
-        })
-    };
-    
-    const handleMouseLeave = () => {
-        setIsHover(false);
-    };
-    
     return(
         <div className="content-technology">
             <h2>Tecnologias</h2>
             <div className='content-technology-r1'>
-                <div 
-                    className={`technology-card ${isHover ? 'technology-card-hover' : ''} `}
-                    onMouseEnter={handleHover}
-                    onMouseLeave={handleMouseLeave}
-                ></div>
-                <div 
-                    className={`technology-card ${isHover ? 'technology-card-hover' : ''} `}
-                    onMouseEnter={handleHover}
-                    onMouseLeave={handleMouseLeave}
-                ></div>  
-                <div 
-                    className={`technology-card ${isHover ? 'technology-card-hover' : ''} `}
-                    onMouseEnter={handleHover}
-                    onMouseLeave={handleMouseLeave}
-                ></div>
-                <div 
-                    className={`technology-card ${isHover ? 'technology-card-hover' : ''} `}
-                    onMouseEnter={handleHover}
-                    onMouseLeave={handleMouseLeave}
-                ></div>              
+
             </div>
         </div>
     )
