8dd453dc4dbfcd0f0d7f8cfd21b372a7739a7b0c
Ian Almeida <iangtaonline1@gmail.com>
2023-08-20 02:47:37 -0300

    add function

--- src/Components/Technology/Technology.jsx
+++ src/Components/Technology/Technology.jsx
@@ -1,17 +1,46 @@
-import './technology.css'
+import './technology.css';
 import React, { useState } from 'react';
 
 function Technology (){
 
-    let [isHover, setIsHover] = useState(false);
+    const [isHover, setIsHover] = useState(false);
+    const [isAnyHover, setIsAnydHover] = useState(false);
+    
+
+    const handleHover = () => {
+        setIsHover(true);
+        setIsAnydHover(true)
+    };
+    
+    const handleMouseLeave = () => {
+        setIsHover(false);
+        setIsAnydHover(false)
+    };
+    
     return(
         <div className="content-technology">
             <h2>Tecnologias</h2>
             <ul className='content-technology-r1'>
-                <li className='technology-card'></li>
-                <li className='technology-card'></li>
-                <li className='technology-card'></li>
-                <li className='technology-card'></li>                
+                <li 
+                    className={`technology-card ${isHover ? 'technology-card-hover' : ''} ${isAnyHover ? 'technology-card-not-hover' : ''}`}
+                    onMouseEnter={handleHover}
+                    onMouseLeave={handleMouseLeave}
+                ></li>
+                <li 
+                    className={`technology-card ${isHover ? 'technology-card-hover' : ''} ${isAnyHover ? 'technology-card-not-hover' : ''}`}
+                    onMouseEnter={handleHover}
+                    onMouseLeave={handleMouseLeave}
+                ></li>  
+                <li 
+                    className={`technology-card ${isHover ? 'technology-card-hover' : ''} ${isAnyHover ? 'technology-card-not-hover' : ''}`}
+                    onMouseEnter={handleHover}
+                    onMouseLeave={handleMouseLeave}
+                ></li>
+                <li 
+                    className={`technology-card ${isHover ? 'technology-card-hover' : ''} ${isAnyHover ? 'technology-card-not-hover' : ''}`}
+                    onMouseEnter={handleHover}
+                    onMouseLeave={handleMouseLeave}
+                ></li>              
             </ul>
         </div>
     )
