e94f3192f121177c3715adccb0c9c71c58663b42
Ian Almeida <iangtaonline1@gmail.com>
2023-08-20 02:26:43 -0300

    add isHovered

--- src/Components/Technology/Technology.jsx
+++ src/Components/Technology/Technology.jsx
@@ -1,8 +1,19 @@
 import './technology.css'
+import React, { useState } from 'react';
 
 function Technology (){
-    return(
 
+    let [isHover, setIsHover] = useState(false);
+    return(
+        <div className="content-technology">
+            <h2>Tecnologias</h2>
+            <ul className='content-technology-r1'>
+                <li className='technology-card'></li>
+                <li className='technology-card'></li>
+                <li className='technology-card'></li>
+                <li className='technology-card'></li>                
+            </ul>
+        </div>
     )
 }
 
