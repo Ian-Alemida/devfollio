9244c4186523af4fa1950d08265c6ed51e405f63
Ian Almeida <iangtaonline1@gmail.com>
2023-08-21 02:02:25 -0300

    create projects

---
+++ src/Components/Projects/Projects.jsx
@@ -0,0 +1,39 @@
+import './Projects.css'
+
+function Projects  () {
+    return (
+        <div className="content-projects">
+            <h2>Projetos <br /><hr /></h2>
+            <div className='content-projects-left'>
+                <div className='content-projects-left-text'>
+                    <h3>Optimus Tech</h3>
+                    <p>Site produzido no workshop "7DaysOfCode" se trata de uma pagina de anuncio  de vagas de uma  empresa cchamada "Optimus Tech". Pude Treinar conceitos de HTML e CSS,  como FlexBox e e display  Grid</p>
+                    <div>
+                        <p>HTML5</p>
+                        <p>CSS3</p>
+                    </div>
+                    <div></div>
+                </div>
+                <div className='content-projects-left-img'>
+
+                </div>
+            </div>
+            <div className='content-projects-left'>
+                <div className='content-projects-rigth-img'>
+
+                </div>
+                <div className='content-projects-rigth-text'>
+                    <h3></h3>
+                    <p></p>
+                    <div>
+                        <p></p>
+                        <p></p>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Projects
