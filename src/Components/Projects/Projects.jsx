294f63219354c02d06656835022f664a67147d91
Ian Almeida <iangtaonline1@gmail.com>
2023-08-25 19:35:10 -0300

    add new project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,6 +4,7 @@ import codeWitheIcon from './images/codeWithe.svg'
 import viewIcon from './images/view.svg'
 import viewWitheIcon from './images/viewWithe.svg'
 import optimusTech from './images/optimusTech.png'
+import alurinha from './images/Alurinha.png'
 import ButtonProject from './ButtonProject/ButonProject'
 
 function Projects  () {
@@ -29,18 +30,18 @@ function Projects  () {
             </div>
             <div className='content-projects-rigth'>
                 <div className='content-projects-rigth-img'>
-                    <img src={optimusTech} alt="imagem do projeto" />
+                    <img src={alurinha} alt="imagem do projeto" />
                 </div>
                 <div className='content-projects-rigth-text'>
-                <h3>Optimus Tech</h3>
-                    <p>Site produzido no workshop "7DaysOfCode" se trata de uma pagina de anuncio  de vagas de uma  empresa chamada "Optimus Tech". Pude Treinar conceitos de HTML e CSS, como FlexBox e display Grid</p>
+                    <h3>Alurinha cursos</h3>
+                    <p>Projeto feito em um dos cursos da Alura com a finalidade de fixar o  conhecimento de flexbox e responsividade para mobile. O projeto consiste em uma das paginas da Alura onde há algumas opções  de curso para o usuario escolher</p>
                     <div>
                         <p>HTML5</p>
                         <p>CSS3</p>
                     </div>
                     <div>
-                        <ButtonProject link="https://github.com/Ian-Alemida/OptimusTech.git" span='ver código' icon1={codeWitheIcon} icon2={codeIcon}></ButtonProject>
-                        <ButtonProject link="https://optimus-tech-indol.vercel.app/" span='ver projeto' icon1={viewWitheIcon} icon2={viewIcon}></ButtonProject>
+                        <ButtonProject link="https://github.com/Ian-Alemida/Alurinha.git" span='ver código' icon1={codeWitheIcon} icon2={codeIcon}></ButtonProject>
+                        <ButtonProject link="https://alurinha-one.vercel.app/" span='ver projeto' icon1={viewWitheIcon} icon2={viewIcon}></ButtonProject>
                     </div>
                 </div>
             </div>
