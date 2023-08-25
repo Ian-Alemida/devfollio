06741ce941222a06f156713688d4bf93d0cae852
Ian Almeida <iangtaonline1@gmail.com>
2023-08-25 19:10:34 -0300

    config rigth project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import './Projects.css'
+import './Style.css'
 import codeIcon from './images/code.svg'
 import codeWitheIcon from './images/codeWithe.svg'
 import viewIcon from './images/view.svg'
@@ -27,18 +27,21 @@ function Projects  () {
                     <img src={optimusTech} alt="imagem do projeto" />
                 </div>
             </div>
-            <div className='content-projects-left'>
+            <div className='content-projects-rigth'>
                 <div className='content-projects-rigth-img'>
-
+                    <img src={optimusTech} alt="imagem do projeto" />
                 </div>
                 <div className='content-projects-rigth-text'>
-                    <h3></h3>
-                    <p></p>
+                <h3>Optimus Tech</h3>
+                    <p>Site produzido no workshop "7DaysOfCode" se trata de uma pagina de anuncio  de vagas de uma  empresa chamada "Optimus Tech". Pude Treinar conceitos de HTML e CSS, como FlexBox e display Grid</p>
                     <div>
-                        <p></p>
-                        <p></p>
+                        <p>HTML5</p>
+                        <p>CSS3</p>
                     </div>
-                    <div></div>
+                    <div>
+                        <ButtonProject link="https://github.com/Ian-Alemida/OptimusTech.git" span='ver código' icon1={codeWitheIcon} icon2={codeIcon}></ButtonProject>
+                        <ButtonProject link="https://optimus-tech-indol.vercel.app/" span='ver projeto' icon1={viewWitheIcon} icon2={viewIcon}></ButtonProject>
+                    </div>
                 </div>
             </div>
         </div>
