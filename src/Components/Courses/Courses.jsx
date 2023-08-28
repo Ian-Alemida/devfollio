a8935be1ad42bbf30f50448780788c6488321551
Ian Almeida <iangtaonline1@gmail.com>
2023-08-28 18:28:59 -0300

    change medidas

--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -9,8 +9,14 @@ function Courses () {
             <div className='content-courses-row'>
                 <CardTecnologo></CardTecnologo>
                 <CardCourses curso='Formação HTML e CSS' plataform='Alura' time='65h - 2022' link='https://cursos.alura.com.br/degree/certificate/41d45351-1690-4a75-b7dc-6a3b2aff321c?lang=pt_BR'></CardCourses>
-                <CardCourses></CardCourses>
-            </div> 
+                <CardCourses curso='Arquitetura de CSS' plataform='Alura' time='8h - 2022' link='https://cursos.alura.com.br/certificate/a2d4daf2-7b26-4d48-9bfe-b6ab0e6d63c7?lang=pt_BR'></CardCourses>
+            </div>
+            <div className='content-courses-row'>
+                <CardCourses curso='Arquitetura de CSS' plataform='Alura' time='8h - 2022' link='https://cursos.alura.com.br/certificate/a2d4daf2-7b26-4d48-9bfe-b6ab0e6d63c7?lang=pt_BR'></CardCourses>
+                <CardCourses curso='Arquitetura de CSS' plataform='Alura' time='8h - 2022' link='https://cursos.alura.com.br/certificate/a2d4daf2-7b26-4d48-9bfe-b6ab0e6d63c7?lang=pt_BR'></CardCourses>
+                <CardCourses curso='Arquitetura de CSS' plataform='Alura' time='8h - 2022' link='https://cursos.alura.com.br/certificate/a2d4daf2-7b26-4d48-9bfe-b6ab0e6d63c7?lang=pt_BR'></CardCourses>
+                <CardCourses curso='Arquitetura de CSS' plataform='Alura' time='8h - 2022' link='https://cursos.alura.com.br/certificate/a2d4daf2-7b26-4d48-9bfe-b6ab0e6d63c7?lang=pt_BR'></CardCourses>
+            </div>
         </div>
     )
 }
