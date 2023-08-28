f43b5a26a4377f3f43e97d17d1311c779f08b869
Ian Almeida <iangtaonline1@gmail.com>
2023-08-28 18:19:58 -0300

    change link

--- src/Components/Courses/Cards/CardCourses.jsx
+++ src/Components/Courses/Cards/CardCourses.jsx
@@ -2,13 +2,11 @@ import  './cardCourses.css'
 
 function CardCourses (props){
     return(
-            <div className='content-cardCourses'>
-                <a href={props.link}>
+            <a className='content-cardCourses' href={props.link} target="_blank" rel='noreferrer noopener'>
                     <h3>Curso</h3>
                     <p className='content-cardCourses-p'>{props.curso} <small>- {props.plataform}</small></p>
                     <span>{props.time}</span>
-                    </a>
-            </div>
+            </a>
     )
 }
 
--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -8,7 +8,7 @@ function Courses () {
             <h2>Formação<span>.</span></h2>
             <div className='content-courses-row'>
                 <CardTecnologo></CardTecnologo>
-                <CardCourses curso='Formação HTML e CSS' plataform='Alura' time='65h - 2022'></CardCourses>
+                <CardCourses curso='Formação HTML e CSS' plataform='Alura' time='65h - 2022' link='https://cursos.alura.com.br/degree/certificate/41d45351-1690-4a75-b7dc-6a3b2aff321c?lang=pt_BR'></CardCourses>
                 <CardCourses></CardCourses>
             </div> 
         </div>
