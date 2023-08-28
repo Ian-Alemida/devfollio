1be6ead674fa88149fbd41980d9d9bc2afcebfb1
Ian Almeida <iangtaonline1@gmail.com>
2023-08-28 16:42:08 -0300

    create card courses

---
+++ src/Components/Courses/Cards/CardCourses.jsx
@@ -0,0 +1,13 @@
+import  './cardCourses.css'
+
+function CardTecnologo (){
+    return(
+        <div className='content-cardTecnologo'>
+            <h3>Tecnólogo</h3>
+            <p className='content-cardTecnologo-p'>Análise e desenvolvimento de sistemas <small>- Descomplica faculdade digital</small></p>
+            <span>2023 - 2025</span>
+        </div>
+    )
+}
+
+export default CardTecnologo;
