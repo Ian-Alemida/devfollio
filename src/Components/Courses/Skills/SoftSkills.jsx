5ec1db786aa225942d1e91171320e9d1c01b32c7
Ian Almeida <iangtaonline1@gmail.com>
2023-08-28 20:22:21 -0300

    create SoftSkills

---
+++ src/Components/Courses/Skills/SoftSkills.jsx
@@ -0,0 +1,31 @@
+import './skills.css'
+
+function SoftSkills () {
+    return(
+        <div className='skills'>
+            <h3>Soft Skills</h3>
+            <div className="skills-row">
+                <p>React.JS</p>
+                <p>HTML5</p>
+                <p>Git & GitHub</p>
+            </div>
+            <div className="skills-row">
+                <p>Node.JS</p>
+                <p>CSS3</p>
+                <p>UX & UI</p>
+            </div>
+            <div className="skills-row">
+                <p>Flutter</p>
+                <p>JavaScript</p>
+                <p>SEO</p>
+            </div>
+            <div className="skills-row">
+                <p>Dart</p>
+                <p>TailWind</p>
+                <p>RegExp</p>
+            </div>
+        </div>
+    )
+}
+
+export default SoftSkills;
