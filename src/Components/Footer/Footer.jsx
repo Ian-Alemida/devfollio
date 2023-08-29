d10e12fab375793aa5aa568ef27beee8f5fffebc
Ian Almeida <iangtaonline1@gmail.com>
2023-08-29 00:38:14 -0300

    create IconText

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
+import Infos from './Infos/Infos';
 import './footer.css'
 
 function Footer () {
     return (
         <div className='Content-footer'>
+            <Infos></Infos>
             <hr />
             <p>© 2023 - Design and code by Ian Almeida.</p>
         </div>
