6fd92d09577d4730ceb1ea39bb0d0fdd0486f2f5
Ian Almeida <iangtaonline1@gmail.com>
2023-08-29 01:10:02 -0300

    download svgs

--- src/Components/Footer/Infos/Infos.jsx
+++ src/Components/Footer/Infos/Infos.jsx
@@ -1,5 +1,7 @@
 import './infos.css'
 import phoneSvg from './Images/phone.svg';
+import mapSvg from './Images/map.svg';
+import emailSvg from './Images/email.svg';
 import IconText from './IconText/IconText';
 
 function Infos () {
@@ -7,6 +9,8 @@ function Infos () {
         <div className='Content-Infos'>
             <div className='Content-Infos-text'>
                 <IconText icon={phoneSvg} text={"+55 (33) 9 9962 4996"}></IconText>
+                <IconText icon={mapSvg} text={"Belo Horizonte - MG"}></IconText>
+                <IconText icon={emailSvg} text={"ian.almeida357@gmail.com"}></IconText>
             </div>
         </div>
     )
