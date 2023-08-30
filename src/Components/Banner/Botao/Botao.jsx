7654c9b0f91314fab2e63647ce10f4a8439c4f12
Ian Almeida <iangtaonline1@gmail.com>
2023-08-30 02:09:54 -0300

    add link

--- src/Components/Banner/Botao/Botao.jsx
+++ src/Components/Banner/Botao/Botao.jsx
@@ -3,10 +3,12 @@ import './botao.css'
 function Botao (props){
 
     return (
-        <button className='animated-button'>
-            <span className='span-text'><img className='icon' src={props.icon} alt="" /> {props.text} </span>
-            <span></span>
-        </button>
+        <a href={props.link} target="_blank" rel='noreferrer noopener'>
+            <button className='animated-button'>
+                <span className='span-text'><img className='icon' src={props.icon} alt="" /> {props.text} </span>
+                <span></span>
+            </button>
+        </a>
     )
 }
 
