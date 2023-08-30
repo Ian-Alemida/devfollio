bc9a5d9ffe6754d93b76eeb5913e2226a95ad894
Ian Almeida <iangtaonline1@gmail.com>
2023-08-30 15:15:58 -0300

    finish nav

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,17 @@
 import Infos from './Infos/Infos';
+import InfosNav from './InfosNav/InfosNav';
 import './footer.css'
 
 function Footer () {
     return (
-        <div className='Content-footer'  id='Footer'>
-            <Infos></Infos>
+        <section className='Content-footer'  id='Footer'>
+            <div className='Content-footer-top'>
+                <Infos></Infos>
+                <InfosNav></InfosNav>
+            </div>
             <hr />
             <p className='Content-footer-copy'>© 2023 - Design and code by Ian Almeida.</p>
-        </div>
+        </section>
     )
 }
 
