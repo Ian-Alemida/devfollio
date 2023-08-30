a4e931b4a2c70af6745a3bca2a8cf22a3119b2dd
Ian Almeida <iangtaonline1@gmail.com>
2023-08-30 20:55:37 -0300

    ajustando tamanhos

--- src/Components/Banner/Botao/Botao.jsx
+++ src/Components/Banner/Botao/Botao.jsx
@@ -3,11 +3,9 @@ import './botao.css'
 function Botao (props){
 
     return (
-        <a href={props.link} target="_blank" rel='noreferrer noopener'>
-            <button className='animated-button'>
+        <a href={props.link} className='animated-button' target="_blank" rel='noreferrer noopener'>
                 <span className='span-text'><img className='icon' src={props.icon} alt="" /> {props.text} </span>
                 <span></span>
-            </button>
         </a>
     )
 }
