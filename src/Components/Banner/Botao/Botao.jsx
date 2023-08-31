bf3e71da86b6060fc3a10afd8ab3b0fb67133094
Ian Almeida <iangtaonline1@gmail.com>
2023-08-30 21:24:02 -0300

    change button

--- src/Components/Banner/Botao/Botao.jsx
+++ src/Components/Banner/Botao/Botao.jsx
@@ -1,10 +1,20 @@
 import './botao.css'
+import React, { useState } from 'react'
 
 function Botao (props){
 
+    const [isHover, setIsHover] = useState(false);
+
+    const handleHover = () => {
+        setIsHover(true);
+    }
+    const handleHoverLeave = () => {
+        setIsHover(false);
+    }
+
     return (
         <a href={props.link} className='animated-button' target="_blank" rel='noreferrer noopener'>
-                <span className='span-text'><img className='icon' src={props.icon} alt="" /> {props.text} </span>
+                <span className='span-text'><img className='icon' src={isHover? props.icon : props.icon2} alt="" /> {props.text} </span>
                 <span></span>
         </a>
     )
