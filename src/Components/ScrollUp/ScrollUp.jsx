9c6f296418eb8593277cc0dea697497c9d55c345
Ian Almeida <iangtaonline1@gmail.com>
2023-08-31 03:14:34 -0300

    customize efects

--- src/Components/ScrollUp/ScrollUp.jsx
+++ src/Components/ScrollUp/ScrollUp.jsx
@@ -37,6 +37,7 @@ function ScrollUp  () {
         <button className={`scollUp-Btn ${isVisible ? '' : 'visible'}`}
         onClick={scrollToTop}>
             <svg height="3vh" className="scollUp-arrow" viewBox="0 0 512 512"><path d="M246.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 109.3 361.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160zm160 352l-160-160c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 301.3 361.4 438.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3z"/></svg>
+            <p className="scollUp-text">Back to Top</p>
         </button>
     );
 }
