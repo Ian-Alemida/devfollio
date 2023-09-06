b7f97a0e9431d307f8936dd8119774cf363453e1
Ian Almeida <iangtaonline1@gmail.com>
2023-09-06 16:53:28 -0300

    ajustes

--- src/Components/Courses/Cards/CardCourses.jsx
+++ src/Components/Courses/Cards/CardCourses.jsx
@@ -1,13 +1,13 @@
-import  './cardCourses.css'
+import './cardCourses.css'
 
-function CardCourses (props){
-    return(
-            <a className='content-cardCourses' href={props.link} target="_blank" rel='noreferrer noopener'>
-                    <h3>Curso</h3>
-                    <p className='content-cardCourses-p'>{props.curso} <small>- {props.plataform}</small></p>
-                    <span>{props.time}</span>
-            </a>
-    )
+function CardCourses(props) {
+        return (
+                <a className='content-cardCourses' href={props.link} target="_blank" rel='noreferrer noopener'>
+                        <h3>Curso</h3>
+                        <p className='content-cardCourses-p'>{props.curso} <small>- {props.plataform}</small></p>
+                        <span>{props.time}</span>
+                </a>
+        )
 }
 
 export default CardCourses;
--- src/Components/ScrollUp/ScrollUp.jsx
+++ src/Components/ScrollUp/ScrollUp.jsx
@@ -8,7 +8,7 @@ function ScrollUp  () {
     //Função que verifica se o mouse está sendo scrolado para baixo ou para  cima, usei callBack para evitar que um novo objeto de função seja criado a cada renderização.
     const handleScroll = useCallback(() => {
         const currentScrollY = window.scrollY;//verifica a posição do  scrollY
-        if (currentScrollY < prevScrollY ) {
+        if (currentScrollY < prevScrollY) {
             setIsVisible(true);
         } else {
             setIsVisible(false);
@@ -30,10 +30,10 @@ function ScrollUp  () {
     }, [handleScroll]);
 
     return ( 
-        <button className={`scollUp-Btn ${isVisible ? '' : 'visible'}`}
+        <button className={`scrollUp-Btn ${isVisible ? '' : 'visible' }`}
         onClick={scrollToTop}>
-            <svg height="3vh" className="scollUp-arrow" viewBox="0 0 512 512"><path d="M246.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 109.3 361.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160zm160 352l-160-160c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 301.3 361.4 438.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3z"/></svg>
-            <p className="scollUp-text">Back to Top</p>
+            <svg height="3vh" className="scrollUp-arrow" viewBox="0 0 512 512"><path d="M246.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 109.3 361.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160zm160 352l-160-160c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 301.3 361.4 438.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3z"/></svg>
+            <p className="scrollUp-text">Back to Top</p>
         </button>
     );
 }
