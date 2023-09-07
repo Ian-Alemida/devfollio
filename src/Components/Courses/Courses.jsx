1704faed80e3876856b52efc9affc5560bf56763
Ian Almeida <iangtaonline1@gmail.com>
2023-09-07 13:22:40 -0300

    rename

--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -13,7 +13,7 @@ function Courses () {
                 <div className='content-courses-row'>
                     <CardTecnologo></CardTecnologo>
                     <CardCourses curso='Formação HTML e CSS' plataform='Alura' time='65h - 2022' link='https://cursos.alura.com.br/degree/certificate/41d45351-1690-4a75-b7dc-6a3b2aff321c?lang=pt_BR'></CardCourses>
-                    <CardCourses curso='JavaScript: Logica de programação' plataform='Alura' time='16h - 2022' link='https://cursos.alura.com.br/certificate/15294591-d288-4137-b240-23b906d2db17?lang=pt_BR'></CardCourses>
+                    <CardCourses curso='JavaScript : Logica de programação' plataform='Alura' time='16h - 2022' link='https://cursos.alura.com.br/certificate/15294591-d288-4137-b240-23b906d2db17?lang=pt_BR'></CardCourses>
                 </div>
                 <div className='content-courses-row'>
                     <CardCourses curso='HTML & CSS parte1 - 4' plataform='Alura' time='36h - 2022' link='https://cursos.alura.com.br/certificate/55b528c5-dfa7-40e1-aea8-249c7048d6a3?lang=pt_BR'></CardCourses>
@@ -36,11 +36,11 @@ function Courses () {
                 <div className='content-courses-row'>
                     <CardCourses curso='Git e GitHub: commit, repositório e versões' plataform='Alura' time='8h - 2022' link='https://cursos.alura.com.br/certificate/ec317584-eaa5-471f-bb72-9fa2369c214f?lang=pt_BR'></CardCourses>
                     <CardCourses curso='Acessibilidade Web parte 1 e 2' plataform='Alura' time='14h - 2022' link='https://cursos.alura.com.br/certificate/9f23fc99-df9d-4dc2-8c49-ed9eb6a93ff9?lang=pt_BR'></CardCourses>
-                    <CardCourses curso='JavaScript: tipos, variáveis e funções' plataform='Alura' time='12h - 2022' link='https://cursos.alura.com.br/certificate/b78d90a3-2ef7-458e-a0f2-87ef4b8a2fe3?lang=pt_BR'></CardCourses>
+                    <CardCourses curso='JavaScript : tipos, variáveis e funções' plataform='Alura' time='12h - 2022' link='https://cursos.alura.com.br/certificate/b78d90a3-2ef7-458e-a0f2-87ef4b8a2fe3?lang=pt_BR'></CardCourses>
                     <CardCourses curso='Expressões regulares: Capturando Textos' plataform='Alura' time='12h - 2022' link='https://cursos.alura.com.br/certificate/d5502a39-efc6-4355-b3d5-fc78a23ddfa9?lang=pt_BR'></CardCourses>
                 </div>
                 <div className='content-courses-row'>
-                    <CardCourses curso='JavaScript: Arrays' plataform='Alura' time='10h - 2022' link='https://cursos.alura.com.br/certificate/851492e3-ad55-4b04-9ad2-834d2a16a8f7?lang=pt_BR'></CardCourses>
+                    <CardCourses curso='JavaScript : Arrays' plataform='Alura' time='10h - 2022' link='https://cursos.alura.com.br/certificate/851492e3-ad55-4b04-9ad2-834d2a16a8f7?lang=pt_BR'></CardCourses>
                     <CardCourses curso='Trabalhando com layouts mobile' plataform='Alura' time='7h - 2022' link='https://cursos.alura.com.br/certificate/ffd81190-8a9f-4d75-b68a-1f86a335f3e1?lang=pt_BR'></CardCourses>
                     <CardCourses curso='CSS Grid: simplificando layouts' plataform='Alura' time='8h - 2022' link='https://cursos.alura.com.br/certificate/5f929371-aba7-4a30-920d-094fc5623e4e?lang=pt_BR'></CardCourses>
                     <CardCourses curso='Flexbox: posicione elementos na tela' plataform='Alura' time='9h - 2022' link='https://cursos.alura.com.br/certificate/8a408a55-9bd5-41d5-b9c9-b330db9e41fa?lang=pt_BR'></CardCourses>
