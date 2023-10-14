229c92ba23af8daacf9f1566b64f374e1d24e033
Ian Almeida <iangtaonline1@gmail.com>
2023-10-13 21:18:30 -0300

    add bootstrap carousel

--- src/Components/Projects/Carousel/Carousel.jsx
+++ src/Components/Projects/Carousel/Carousel.jsx
@@ -1,5 +1,35 @@
-const Slider = () => {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import img from "../images/PlanetSky.png"
+import Carousel from 'react-bootstrap/Carousel';
 
-};
+function Slider() {
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <img src={img} alt="" srcset="" />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+            <img src={img} alt="" srcset="" />
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+            <img src={img} alt="" srcset="" />
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+}
 
 export default Slider;
