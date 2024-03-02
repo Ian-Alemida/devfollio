20fa2eb22a1b6247ef5e478cafd092f94d745d3a
Ian Almeida <iangtaonline1@gmail.com>
2024-03-02 18:15:04 -0300

    Adicionando mecanismo de filtrar por categoria

--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -4,49 +4,37 @@ import CardCourses from './Cards/CardCourses';
 import HardSkills from './Skills/HardSkills';
 import SoftSkills from './Skills/SoftSkills';
 import Idiomas from './Skills/Idiomas';
+import { useState } from 'react';
 
 function Courses({ cursos }) {
+    const [skillActive, setSkillActive] = useState('front-end')
+
+    const Skills =['Front-end','Back-end','Cyber Security','Faculdade','Mobile', 'Livros']
+
+    const skillVisible = skillActive ? 
+    cursos.filter(curso => curso.skill === skillActive) : 
+    cursos;
+
     return (
         <section className='content-courses' id='Courses'>
             <h2>Formação<span>.</span></h2>
             <div className='content-courses-menu'>
-                <button>Front-End</button>
-                <button>Back-End</button>
-                <button>Cyber Security</button>
-                <button>Faculdade</button>
-                <button>Mobile</button>
+                {Skills.map((skill) => {
+                    return <button value={skill} onClick={() => {setSkillActive(skill)}}>
+                                {skill}
+                            </button>
+                })}
             </div>
             <article>
                 <ul className='cards'>
-                    <CardTecnologo 
-                        type={'Tecnólogo'} 
-                        nome={'Análise e desenvolvimento de sistemas'}
-                        instituição={'Descomplica faculdade digital'}
-                        time={'2023 - 2025'}
-                        link={'###'}
-                    />
-                    <CardTecnologo 
-                        type={'Certificado'}
-                        nome={'Desenvolvedor front-end'}
-                        instituição={'Descomplica faculdade digital'}
-                        time={'240h - 2023'}
-                        link={'https://certificados.descomplica.com.br/graduacao/1d59a0dcadafb2fe77329ced4eff1b5de58b72bbc4b4bcde8cf3ed3ff4bcc40e'}
-                    />
-                    <CardTecnologo 
-                        type={'Certificado'}
-                        nome={'Programmer'}
-                        instituição={'Descomplica faculdade digital'}
-                        time={'160h - 2023'}
-                        link={'https://certificados.descomplica.com.br/graduacao/13a65a3e574b4da5b7163364671b81e3c215ee0ab04a800319684dd3e844992b'}
-                    />
-                    {cursos.map((curso, indice) => {
-                        return <CardCourses
-                            key={indice}
-                            link={curso.link}
-                            nome={curso.nome}
-                            plataform={curso.plataform}
-                            time={curso.time}
-                        />
+                    {skillVisible.map((curso, indice) => {                                       
+                        return  <CardCourses
+                                    key={indice}
+                                    link={curso.link}
+                                    nome={curso.nome}
+                                    plataform={curso.plataform}
+                                    time={curso.time}
+                                />  
                     })}
                 </ul>
             </article>
