ad833a03fd2b3e3f82f892fd1146fb414d27823e
Ian Almeida <iangtaonline1@gmail.com>
2024-03-02 20:19:17 -0300

    organizando DB

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,9 @@ import ScrollUp from './Components/ScrollUp/ScrollUp';
 
 function App() {
 
-  const cursos = [{
+  const cursos = [
+    //Faculdade
+    {
     "type": 'Tecnólogo',
     "nome": 'Análise e desenvolvimento de sistemas',
     "plataform": 'Descomplica faculdade digital',
@@ -19,7 +21,7 @@ function App() {
     "link": '###',
     "skill": 'Faculdade'
   },
-  {
+  { 
     "type": 'Certificado',
     "nome": 'Desenvolvedor front-end',
     "plataform": 'Descomplica faculdade digital',
@@ -35,6 +37,7 @@ function App() {
     "link": 'https://certificados.descomplica.com.br/graduacao/13a65a3e574b4da5b7163364671b81e3c215ee0ab04a800319684dd3e844992b',
     "skill": 'Faculdade'
   },
+  //Front-end ----------------------------------------------------------
   {
     "nome": 'React: desenvolvendo com JavaScript',
     "plataform": 'Alura',
@@ -55,13 +58,6 @@ function App() {
     "time": '65h - 2022',
     "link": 'https://cursos.alura.com.br/degree/certificate/41d45351-1690-4a75-b7dc-6a3b2aff321c?lang=pt_BR',
     "skill": 'Front-end'
-  },
-  {
-    "nome": 'JavaScript : Logica de programação',
-    "plataform": 'Alura',
-    "time": '16h - 2022',
-    "link": 'https://cursos.alura.com.br/certificate/15294591-d288-4137-b240-23b906d2db17?lang=pt_BR',
-    "skill": 'Back-end'
   },
   {
     "nome": 'HTML & CSS parte1 - 4',
@@ -85,103 +81,12 @@ function App() {
     "skill": 'Front-end'
   },
   {
-    "nome": "Responsividade com mobile-first",
-    "plataform": "Alura",
-    "time": "12h - 2022",
-    "link": "https://cursos.alura.com.br/certificate/bd20bb5d-d697-4d48-96d8-770a63a2d555?lang=pt_BR",
-    "skill": 'Mobile'
-  },
-  {
-    "nome": "Flutter: Widgets, Imagens e Animações",
-    "plataform": "Alura",
-    "time": "16h - 2022",
-    "link": "https://cursos.alura.com.br/certificate/ea9ecb16-8183-4cb6-a4ee-5ac44266a4e4?lang=pt_BR",
-    "skill": 'Mobile'
-  },
-  {
-    "nome": "Flutter: Controller, navegação e estados",
-    "plataform": "Alura",
-    "time": "10h - 2022",
-    "link": "https://cursos.alura.com.br/certificate/e0756453-0b33-4f7f-9f53-7955d1f269e2?lang=pt_BR",
-    "skill": 'Mobile'
-  },
-  {
-    "nome": "Flutter: aplicando persistência de dados",
-    "plataform": "Alura",
-    "time": "10h - 2022",
-    "link": "https://cursos.alura.com.br/certificate/cec59a3d-2c00-483a-8e9c-462136548588?lang=pt_BR",
-    "skill": 'Mobile'
-  },
-  {
-    "nome": "Flutter com WebAPI: integrando aplicações",
-    "plataform": "Alura",
-    "time": "10h - 2022",
-    "link": "https://cursos.alura.com.br/certificate/1a1f7a1a-84d1-4018-8f53-2b290a6f0dbf?lang=pt_BR",
-    "skill": 'Mobile'
-  },
-  {
-    "nome": "Dart: entendendo a Orientação a Objetos",
-    "plataform": "Alura",
-    "time": "12h - 2022",
-    "link": "https://cursos.alura.com.br/certificate/a4d56fcb-f43e-4675-b939-45e383a4433f?lang=pt_BR",
-    "skill": 'Back-end'
-  },
-  {
-    "nome": "Dart: lidando com Exceptions e Null Safety",
-    "plataform": "Alura",
-    "time": "12h - 2022",
-    "link": "https://cursos.alura.com.br/certificate/db593eb5-2a1f-44fb-9dff-3396a8ce4ec7?lang=pt_BR",
-    "skill": 'Back-end'
-  },
-  {
-    "nome": "Dart: sintaxe, coleções e dinamismo",
-    "plataform": "Alura",
-    "time": "8h - 2022",
-    "link": "https://cursos.alura.com.br/certificate/5d0853bb-737a-4fa7-85d6-51a74a05d2b5?lang=pt_BR",
-    "skill": 'Back-end'
-  },
-  {
-    "nome": "Dart: Manipulando variáveis e listas",
-    "plataform": "Alura",
-    "time": "10h - 2022",
-    "link": "https://cursos.alura.com.br/certificate/380932f7-d728-4456-8cbc-b5c4bb79a641?lang=pt_BR",
-    "skill": 'Back-end'
-  },
-  {
-    "nome": "Git e GitHub: commit, repositório e versões",
-    "plataform": "Alura",
-    "time": "8h - 2022",
-    "link": "https://cursos.alura.com.br/certificate/ec317584-eaa5-471f-bb72-9fa2369c214f?lang=pt_BR",
-    "skill": 'Back-end'
-  },
-  {
     "nome": "Acessibilidade Web parte 1 e 2",
     "plataform": "Alura",
     "time": "14h - 2022",
     "link": "https://cursos.alura.com.br/certificate/9f23fc99-df9d-4dc2-8c49-ed9eb6a93ff9?lang=pt_BR",
     "skill": 'Front-end'
-  },
-  {
-    "nome": "JavaScript : tipos, variáveis e funções",
-    "plataform": "Alura",
-    "time": "12h - 2022",
-    "link": "https://cursos.alura.com.br/certificate/b78d90a3-2ef7-458e-a0f2-87ef4b8a2fe3?lang=pt_BR",
-    "skill": 'Back-end'
-  },
-  {
-    "nome": "Expressões regulares: Capturando Textos",
-    "plataform": "Alura",
-    "time": "12h - 2022",
-    "link": "https://cursos.alura.com.br/certificate/d5502a39-efc6-4355-b3d5-fc78a23ddfa9?lang=pt_BR",
-    "skill": 'Back-end'
-  },
-  {
-    "nome": "JavaScript : Arrays",
-    "plataform": "Alura",
-    "time": "10h - 2022",
-    "link": "https://cursos.alura.com.br/certificate/851492e3-ad55-4b04-9ad2-834d2a16a8f7?lang=pt_BR",
-    "skill": 'Back-end'
-  },
+  }, 
   {
     "nome": "Trabalhando com layouts mobile",
     "plataform": "Alura",
@@ -229,6 +134,106 @@ function App() {
     "time": "8h - 2022",
     "link": "https://cursos.alura.com.br/certificate/a2d4daf2-7b26-4d48-9bfe-b6ab0e6d63c7?lang=pt_BR",
     "skill": 'Front-end'
+  },
+  //Back-End ---------------------------------------------------------------
+  {
+    "nome": 'JavaScript : Logica de programação',
+    "plataform": 'Alura',
+    "time": '16h - 2022',
+    "link": 'https://cursos.alura.com.br/certificate/15294591-d288-4137-b240-23b906d2db17?lang=pt_BR',
+    "skill": 'Back-end'
+  },
+  {
+    "nome": "Dart: entendendo a Orientação a Objetos",
+    "plataform": "Alura",
+    "time": "12h - 2022",
+    "link": "https://cursos.alura.com.br/certificate/a4d56fcb-f43e-4675-b939-45e383a4433f?lang=pt_BR",
+    "skill": 'Back-end'
+  },
+  {
+    "nome": "Dart: lidando com Exceptions e Null Safety",
+    "plataform": "Alura",
+    "time": "12h - 2022",
+    "link": "https://cursos.alura.com.br/certificate/db593eb5-2a1f-44fb-9dff-3396a8ce4ec7?lang=pt_BR",
+    "skill": 'Back-end'
+  },
+  {
+    "nome": "Dart: sintaxe, coleções e dinamismo",
+    "plataform": "Alura",
+    "time": "8h - 2022",
+    "link": "https://cursos.alura.com.br/certificate/5d0853bb-737a-4fa7-85d6-51a74a05d2b5?lang=pt_BR",
+    "skill": 'Back-end'
+  },
+  {
+    "nome": "Dart: Manipulando variáveis e listas",
+    "plataform": "Alura",
+    "time": "10h - 2022",
+    "link": "https://cursos.alura.com.br/certificate/380932f7-d728-4456-8cbc-b5c4bb79a641?lang=pt_BR",
+    "skill": 'Back-end'
+  },
+  {
+    "nome": "Git e GitHub: commit, repositório e versões",
+    "plataform": "Alura",
+    "time": "8h - 2022",
+    "link": "https://cursos.alura.com.br/certificate/ec317584-eaa5-471f-bb72-9fa2369c214f?lang=pt_BR",
+    "skill": 'Back-end'
+  },
+  {
+    "nome": "JavaScript : tipos, variáveis e funções",
+    "plataform": "Alura",
+    "time": "12h - 2022",
+    "link": "https://cursos.alura.com.br/certificate/b78d90a3-2ef7-458e-a0f2-87ef4b8a2fe3?lang=pt_BR",
+    "skill": 'Back-end'
+  },
+  {
+    "nome": "Expressões regulares: Capturando Textos",
+    "plataform": "Alura",
+    "time": "12h - 2022",
+    "link": "https://cursos.alura.com.br/certificate/d5502a39-efc6-4355-b3d5-fc78a23ddfa9?lang=pt_BR",
+    "skill": 'Back-end'
+  },
+  {
+    "nome": "JavaScript : Arrays",
+    "plataform": "Alura",
+    "time": "10h - 2022",
+    "link": "https://cursos.alura.com.br/certificate/851492e3-ad55-4b04-9ad2-834d2a16a8f7?lang=pt_BR",
+    "skill": 'Back-end'
+  },
+  //Mobile -----------------------------------------------
+  {
+    "nome": "Responsividade com mobile-first",
+    "plataform": "Alura",
+    "time": "12h - 2022",
+    "link": "https://cursos.alura.com.br/certificate/bd20bb5d-d697-4d48-96d8-770a63a2d555?lang=pt_BR",
+    "skill": 'Mobile'
+  },
+  {
+    "nome": "Flutter: Widgets, Imagens e Animações",
+    "plataform": "Alura",
+    "time": "16h - 2022",
+    "link": "https://cursos.alura.com.br/certificate/ea9ecb16-8183-4cb6-a4ee-5ac44266a4e4?lang=pt_BR",
+    "skill": 'Mobile'
+  },
+  {
+    "nome": "Flutter: Controller, navegação e estados",
+    "plataform": "Alura",
+    "time": "10h - 2022",
+    "link": "https://cursos.alura.com.br/certificate/e0756453-0b33-4f7f-9f53-7955d1f269e2?lang=pt_BR",
+    "skill": 'Mobile'
+  },
+  {
+    "nome": "Flutter: aplicando persistência de dados",
+    "plataform": "Alura",
+    "time": "10h - 2022",
+    "link": "https://cursos.alura.com.br/certificate/cec59a3d-2c00-483a-8e9c-462136548588?lang=pt_BR",
+    "skill": 'Mobile'
+  },
+  {
+    "nome": "Flutter com WebAPI: integrando aplicações",
+    "plataform": "Alura",
+    "time": "10h - 2022",
+    "link": "https://cursos.alura.com.br/certificate/1a1f7a1a-84d1-4018-8f53-2b290a6f0dbf?lang=pt_BR",
+    "skill": 'Mobile'
   }]
 
   return (
