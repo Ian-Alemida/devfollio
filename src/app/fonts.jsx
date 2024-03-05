19889877c1d6b8fd8619f1c8c7682aa3aec39ec5
Ian Almeida <iangtaonline1@gmail.com>
2024-03-05 17:40:26 -0300

    Adicionando fontes ao projeto

---
+++ src/app/fonts.jsx
@@ -0,0 +1,14 @@
+import {Ubuntu_Mono, Roboto, Merriweather} from 'next/font/google'
+
+export const Ubuntu = Ubuntu_Mono({
+    weight: ['400','700'],
+    subsets: ['latin'],
+});
+export const roboto_font = Roboto({
+    weight: ['100','400','700'],
+    subsets: ['latin'],
+});
+export const merriweather_font = Merriweather({
+    weight: ['300','400','700'],
+    subsets: ['latin'],
+});
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Ubuntu} from "@/app/fonts";
 
 export const metadata = {
   title: "Create Next App",
@@ -8,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body className={Ubuntu.className}>{children}</body>
     </html>
   );
 }
