aa7b89067ec1e4b3520b95fdc322e2a31069522e
Ian Almeida <iangtaonline1@gmail.com>
2024-03-05 11:03:46 -0300

    Passando softSkills para uma variavel e customizando

--- src/app/Components/Courses/Skills/SoftSkills.jsx
+++ src/app/Components/Courses/Skills/SoftSkills.jsx
@@ -1,28 +1,15 @@
 import './skills.css'
 
-function SoftSkills () {
-    return(
+function SoftSkills() {
+
+    const skills = ['Resolução de problemas', 'Comunicação eficaz', 'Flexibilidade', 'Rápida aprendizagem', 'Escuta ativa', 'Criatividade', 'Disciplina para seguir protocolos', 'Trabalho sob pressão', 'Curiosidade', 'Trabalho em equipe', 'Adaptação']
+    return (
         <div className='skills'>
             <h3>Soft Skills</h3>
-            <div className="skills-row">
-                <p>Resolução de problemas</p>
-                <p>Comunicação eficaz</p>
-                <p>Flexibilidade</p>
-            </div>
-            <div className="skills-row">
-                <p>Rápida aprendizagem</p>
-                <p>Escuta ativa</p>
-                <p>Criatividade</p>
-            </div>
-            <div className="skills-row">
-                <p>Disciplina para seguir protocolos</p>
-                <p>Trabalho sob pressão</p>
-                <p>Curiosidade</p>
-            </div>
-            <div className="skills-row">
-                <p>Trabalho em equipe</p>
-                <p>Adaptação</p>              
-                <p></p>
+            <div className="skills-list">
+                {skills.map((skill, indice) => {
+                    return <p key={indice}>{skill}</p>
+                })}
             </div>
         </div>
     )
