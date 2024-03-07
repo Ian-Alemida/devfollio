00ee5cf3cbba585337eca05796ecef21ecbe3ee9
Ian Almeida <iangtaonline1@gmail.com>
2024-03-07 11:08:08 -0300

    ajuste

--- src/app/Components/Banner/Banner.jsx
+++ src/app/Components/Banner/Banner.jsx
@@ -6,26 +6,26 @@ import iconLinkedinBlack from './images/linkedinBlack.svg'
 import iconGithub from './images/github.svg'
 import iconGithubBlack from './images/githubBlack.svg'
 import ImgBanner from './ImgBanner/ImgBanner'
-import { roboto} from "@/app/fonts"
+import { roboto, ubuntu} from "@/app/fonts" 
 
 
 function Banner (){
     return (
         <div className='content' id='Banner'>
             <div className='content-text'>
-                <div class="content-animation">
-                    <div class="content-animation__container">    
-                        <ul class="content-animation__container__list">
-                            <li class="content-animation__container__list__item">Hello world !</li>
-                            <li class="content-animation__container__list__item">Olá mundo !</li>
+                <div className="content-animation">
+                    <div className="content-animation__container">    
+                        <ul className="content-animation__container__list">
+                            <li className="content-animation__container__list__item">Hello world !</li>
+                            <li className="content-animation__container__list__item">Olá mundo !</li>
                         </ul>
                     </div>
                 </div>
                 <h2 className={roboto.className}>Prazer, Eu sou Ian Almeida<span>.</span></h2>
-                <p className={`${roboto.className} animation`}>Sou um desenvolvedor Front-End...</p>
+                <p className={`${ubuntu.className} animation`}>Sou um desenvolvedor Front-End...</p>
                 <div className='content-button'>
                     <Botao icon1={iconLinkedin} icon2={iconLinkedinBlack} link="https://www.linkedin.com/in/ian-almeida-3a707a240/" text={'Linkedin'}></Botao>
-                    <Botao  icon1={iconGithub} icon2={iconGithubBlack} link="https://github.com/Ian-Alemida" text={'GitHub'} class={"content-button-git"}></Botao>
+                    <Botao  icon1={iconGithub} icon2={iconGithubBlack} link="https://github.com/Ian-Alemida" text={'GitHub'} className={"content-button-git"}></Botao>
                 </div>
             </div>
             <ImgBanner></ImgBanner>
