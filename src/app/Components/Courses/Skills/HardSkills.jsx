ee8f4f3c5783a39e303a1e086a3b2f81f88ba604
Ian Almeida <iangtaonline1@gmail.com>
2024-03-09 16:44:58 -0300

    ajustando css

--- src/app/Components/Courses/Skills/HardSkills.jsx
+++ src/app/Components/Courses/Skills/HardSkills.jsx
@@ -2,7 +2,7 @@ import './skills.css'
 
 function HardSkills() {
 
-    const skills = ['Next.js','React.JS','Prompt Engineer', 'Ethical Hacking', 'Java', 'Node.JS', 'Git & GitHub', 'Pentest', 'CSS3','HTML5', 'UX & UI', 'JavaScript','BootsTrap', 'SEO', 'Flutter', 'TailWind', 'RegExp', 'Dart']
+    const skills = ['Next.js','React.JS','Prompt Engineer', 'Ethical Hacking', 'Java', 'Node.JS', 'Git & GitHub', 'Pentest','styled-components', 'CSS3','HTML5', 'UX & UI', 'JavaScript','BootsTrap', 'SEO-Optimization', 'Flutter', 'TailWind', 'Dart']
     return (
         <div className='skills'>
             <h3>Hard Skills</h3>
