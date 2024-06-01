3b0edab695ccc337f72fdf26b73ff34d31fc7b92
W7(7)H|6S%32\L7F=5L53H+G(BS/aq&$00! <ian-alemida>
2024-06-01 17:04:22 -0300

    alterando caminhos das imagens

--- src/app/Components/Technology/Technology.jsx
+++ src/app/Components/Technology/Technology.jsx
@@ -1,15 +1,5 @@
 import CardTechnology from './CardTechnology/CardTechnology';
 import './technology.css';
-import gitIcon from './images/git.svg'
-import htmlIcon from './images/html5.svg'
-import cssIcon from './images/css3.svg'
-import jsIcon from './images/js.svg'
-import reactIcon from './images/react.svg'
-import nodeIcon from './images/node.svg'
-import tailwindIcon from './images/tailwind.png'
-import flutterIcon from './images/flutter.png'
-import nextIcon from './images/Next.js.svg'
-import javaIcon from './images/java.svg'
 import { roboto } from "@/app/fonts"
 
 
@@ -19,52 +9,52 @@ function Technology() {
 
     const Tecnologias = [
         {
-            "img": htmlIcon,
+            "img": '/assets/images/technology/html5.svg',
             "nome": "HTML5",
             "text": "HTML é uma linguagem de marcação utilizada na construção de paginas web."
         },
         {
-            "img": cssIcon,
+            "img": '/assets/images/technology/css3.svg',
             "nome": "CSS3",
             "text": "Utilizado  para estilizar elementos  escritos em linguagem de marcação."
         },
         {
-            "img": jsIcon,
+            "img": '/assets/images/technology/js.svg',
             "nome": "JavaScript",
             "text": "Utilizo JavaScript para criar aplicações com comportamento interativo e complexo."
         },
         {
-            "img": reactIcon,
+            "img": '/assets/images/technology/react.svg',
             "nome": "React.JS",
             "text": "Biblioteca usada para proporcionar interatividade ao usuário e componetizar a pagina."
         },
         {
-            "img": nextIcon,
+            "img": '/assets/images/technology/Next.js.svg',
             "nome": "Next.JS",
             "text": "Utilizo next para proporcionar ao usuário uma experiencia mais rápida e melhorar o SEO."
         },
         {
-            "img": tailwindIcon,
+            "img": '/assets/images/technology/tailwind.png',
             "nome": "Tailwind",
             "text": "Tailwind é um FrameWork CSS utilizado para estilizar os elementos através de classes."
         },
         {
-            "img": flutterIcon,
+            "img": '/assets/images/technology/flutter.png',
             "nome": "Flutter",
             "text": "Utilizo Flutter juntamente com Dart para criação de telas em dispositivos mobile."
         },
         {
-            "img": gitIcon,
+            "img": '/assets/images/technology/git.svg',
             "nome": "Git e GitHub",
             "text": "Utilizo o git como programa de versionamento das minhas aplicações."
         },
         {
-            "img": javaIcon,
+            "img": '/assets/images/technology/java.svg',
             "nome": "Java",
             "text": "Utilizo Java no Back-end e para a criação de algumas API's."
         },
         {
-            "img": nodeIcon,
+            "img": '/assets/images/technology/node.svg',
             "nome": "Node.JS",
             "text": "Utilizo Node.JS para criação de API's no padrão REST."
         }
