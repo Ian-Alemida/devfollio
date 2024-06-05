385db6728026256d7342adec818d0b9e39f4b33c
ian-alemida <iangtaonline1@gmail.com>
2024-06-04 23:33:24 -0300

    conectando ao mongo dp

---
+++ src/db/db.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const uri =
+  "mongodb+srv://iandb:ckoimKd9u6amibgF@cluster0.ogzyvdv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const DB_CONNECT = process.env.MONGODB_URI || uri;
+
+async function connectDB() {
+  try {
+    await mongoose.connect(DB_CONNECT, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Conectado ao MongoDB Atlas!");
+  } catch (error) {
+    console.error("Erro ao conectar ao MongoDB Atlas:", error);
+    process.exit(1);
+  }
+}
+
+module.exports = {
+  connectDB,
+};
+
+/*const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function getCollectionData() {
+    try {
+        // Conectar ao MongoDB
+        await client.connect();
+        console.log('Conectado ao MongoDB...');
+
+        // Selecionar o banco de dados e a coleção
+        const database = client.db('devfollio');
+        const collection = database.collection('tecnologias');
+
+        // Executar a consulta (por exemplo, encontrar todos os documentos na coleção)
+        const query = {};
+        const result = await collection.find(query).toArray();
+        console.log(result)
+        return result;
+    } catch (e) {
+        console.error('Erro ao conectar ao MongoDB:', e);
+    } finally {
+        // Fechar a conexão com o MongoDB
+        await client.close();
+        console.log('Conexão com o MongoDB fechada.');
+    }
+}
+module.exports = { getCollectionData };
+console.log(getCollectionData())*/
