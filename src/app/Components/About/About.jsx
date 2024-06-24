f9fb90679643ba632e60b244aee8c5375eeafe74
ian-alemida <iangtaonline1@gmail.com>
2024-06-23 23:38:44 -0300

    trocando icones

--- src/app/Components/About/About.jsx
+++ src/app/Components/About/About.jsx
@@ -3,7 +3,9 @@ import perfil from './images/perfil.jpg'
 import perfil2 from './images/perfil2.png'
 import Botao from '../Banner/Botao/Botao'
 import iconDownload from './images/download-icon.svg'
+import iconDownloadGray from './images/downloadGray.svg'
 import instagramIcon from './images/instagram-icon.svg'
+import instagramGrayIcon from './images/instagramGray.svg'
 import Card from './Card/Card'
 import Image from 'next/image'
 import Curriculo from './Currículo-IanAlmeida.pdf'
@@ -22,8 +24,8 @@ function About() {
                 <p className='content-about-text-p'>Desde 2022, tenho me dedicado ao desenvolvimento de software, e essa jornada tem sido verdadeiramente apaixonante. Cresci fascinado pela tecnologia e, desde cedo, tive interesse na computação. Em 2023, decidi dar um passo adiante e me matriculei no curso de Análise e Desenvolvimento de Sistemas, buscando aprimorar minhas habilidades e me manter em constante desenvolvimento. </p>
                 <p className='content-about-text-p'>Estou comprometido em aprimorar minhas habilidades e contribuir com meu conhecimento para ajudar a comunidade alcançar seus objetivos.</p>
                 <div className='content-button'>
-                    <Botao icon1={iconDownload} icon2={iconDownload} link={Curriculo} isDownload text={'Baixar CV'}></Botao>
-                    <Botao icon1={instagramIcon} icon2={instagramIcon} link="https://www.instagram.com/binary_jouney/?igshid=OGQ5ZDc2ODk2ZA=='" text={'Instagram'}></Botao>
+                    <Botao icon1={iconDownloadGray} icon2={iconDownload} link={Curriculo} isDownload text={'Baixar CV'}></Botao>
+                    <Botao icon1={instagramGrayIcon} icon2={instagramIcon} link="https://www.instagram.com/binary_jouney/?igshid=OGQ5ZDc2ODk2ZA=='" text={'Instagram'}></Botao>
                 </div>
             </div>
         </div>
