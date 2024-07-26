ca55cc633e359fce328ee59b4a88a8669736557e
ian-alemida <iangtaonline1@gmail.com>
2024-07-26 19:59:24 -0300

    estruturando modal do chat

--- src/app/Components/ChatUI/ChatUI.jsx
+++ src/app/Components/ChatUI/ChatUI.jsx
@@ -1,11 +1,42 @@
 import { useState, useEffect } from 'react';
 import { styled } from "styled-components"
 
+const ChatModal = styled.div`
+    background-color: var(--cinza-medio);
+    color: black;
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: center;  
+    position: fixed;
+    z-index: 2;
+    right: 3vw;
+    bottom: 12vh;
+    padding-bottom: 21px;
+    width: 520px;
+    height: 660px;
+`
+
+const MessageBubble = styled.div`
+    width: 90%;
+    height: 42px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-radius: 18px;
+    background-color: white;
+`
+
+const TextMessage = styled.p`
+    margin: 0 1rem;
+
+`
 
 export default function ChatUI() {
     return (
-        <modal>
-            <p>hello</p>
-        </modal>
+        <ChatModal>
+            <MessageBubble>
+                <TextMessage>hello</TextMessage>
+            </MessageBubble>
+        </ChatModal>
     )
 }
