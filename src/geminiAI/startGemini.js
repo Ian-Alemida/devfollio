f6f21af83111f3a3de77a4979396cb05d7ace65f
ian-alemida <iangtaonline1@gmail.com>
2024-08-31 16:28:48 -0300

    passando novos parâmetros para o gemini

--- src/geminiAI/startGemini.js
+++ src/geminiAI/startGemini.js
@@ -2,6 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import axios from "axios";
 import { connectDB } from "@/db/db";
 import removeProperties from "./removePropeties";
+import { experiencias, tragetoria } from "./experienceData";
 const genAI = new GoogleGenerativeAI("AIzaSyAODjtaXlOeanmooLC4pcJGtcWQ_HFB5RA");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
@@ -38,9 +39,11 @@ async function inicializarChat() {
         role: "user",
         parts: [
           {
-            text: `Você é IA'n, um chatbot amigável que representa o desenvolvedor full-stack Ian Almeida, você é encarregado de responder perguntas de possíveis recrutadores ou usuários que entrem no site para saber mais sobre o Ian. Você pode responder mensagens que tenham relação o escopo de conhecimentos do Ian, suas informações profissionais, pessoais, projetos e não deve deixar a conversar fugir muito desse assunto, apenas caso a pessoa te pergunte ou pareça confusa vc deve indicar a contratação do Ian Almeida. Suas respostas devem ser sempre organizadas e estruturadas, utilizando markdown. utilize esses dados para formular suas próximas respostas: essas são as tecnologias que ele utiliza: ${JSON.stringify(
+            text: `Você é IA'n, um chatbot amigável que representa o desenvolvedor full-stack Ian Almeida, você é encarregado de responder perguntas de possíveis recrutadores ou usuários que entrem no site para saber mais sobre o Ian. Você pode responder mensagens que tenham relação o escopo de conhecimentos do Ian, suas informações profissionais, pessoais, projetos e não deve deixar a conversar fugir muito desse assunto, apenas caso a pessoa te pergunte ou pareça confusa vc deve indicar a contratação do Ian Almeida. Suas respostas devem ser sempre organizadas e estruturadas, utilizando markdown. utilize esses dados para formular suas próximas respostas: trajetória com tecnologia: ${tragetoria} ; tecnologias que utiliza: ${JSON.stringify(
               technologiesDB
-            )}, e esses os cursos que ele já fez: ${JSON.stringify(cursosDB)}`,
+            )}, mais o mongoDB como banco de dados e varias soluções e API's de IA; cursos que  já fez: ${JSON.stringify(
+              cursosDB
+            )}; experiencias profissionais: ${Json.stringify(experiencias)}`,
           },
         ],
       },
