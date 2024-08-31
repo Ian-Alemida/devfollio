db2dde5235d09f090857958e0fbdb3241f92dec2
ian-alemida <iangtaonline1@gmail.com>
2024-08-31 14:03:06 -0300

    trazendo os dados para o gemini

--- src/geminiAI/startGemini.js
+++ src/geminiAI/startGemini.js
@@ -2,20 +2,24 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import axios from "axios";
 import { connectDB } from "@/db/db";
 import removeProperties from "./removePropeties";
-
 const genAI = new GoogleGenerativeAI("AIzaSyAODjtaXlOeanmooLC4pcJGtcWQ_HFB5RA");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 let chat;
 let cursosDB;
+let technologiesDB;
 
 async function getData() {
   try {
     await connectDB();
 
-    const response = await axios.get("http://localhost:3000/api/getCourses");
-    console.log(removeProperties(response.data));
-    cursosDB = removeProperties(response.data);
+    cursosDB = await axios
+      .get("http://localhost:3000/api/getCourses")
+      .then((response) => removeProperties(response.data));
+
+    technologiesDB = await axios
+      .get("http://localhost:3000/api/getTechnologies")
+      .then((response) => removeProperties(response.data));
   } catch (error) {
     console.error(
       "Erro ao fazer a requisição:",
@@ -24,9 +28,9 @@ async function getData() {
   }
 }
 
-function inicializarChat() {
-  getData();
+await getData();
 
+async function inicializarChat() {
   // Inicializa o chat passando  um histórico de conversa para guiar as próximas ações do chatbot
   chat = model.startChat({
     history: [
@@ -34,7 +38,9 @@ function inicializarChat() {
         role: "user",
         parts: [
           {
-            text: "Você é IA'n, um chatbot amigável que representa o desenvolvedor full-stack Ian Almeida, você é encarregado de responder perguntas de possíveis recrutadores ou usuários que entrem no site para saber mais sobre o Ian. Você pode responder mensagens que tenham relação o escopo de conhecimentos do Ian, suas informações profissionais, pessoais, projetos e não deve deixar a conversar fugir muito desse assunto, caso a pessoa te pergunte vc sempre deve indicar a contratação do Ian Almeida. Suas respostas devem ser sempre organizadas e estruturadas, utilizando markdown",
+            text: `Você é IA'n, um chatbot amigável que representa o desenvolvedor full-stack Ian Almeida, você é encarregado de responder perguntas de possíveis recrutadores ou usuários que entrem no site para saber mais sobre o Ian. Você pode responder mensagens que tenham relação o escopo de conhecimentos do Ian, suas informações profissionais, pessoais, projetos e não deve deixar a conversar fugir muito desse assunto, apenas caso a pessoa te pergunte ou pareça confusa vc deve indicar a contratação do Ian Almeida. Suas respostas devem ser sempre organizadas e estruturadas, utilizando markdown. utilize esses dados para formular suas próximas respostas: essas são as tecnologias que ele utiliza: ${JSON.stringify(
+              technologiesDB
+            )}, e esses os cursos que ele já fez: ${JSON.stringify(cursosDB)}`,
           },
         ],
       },
@@ -49,7 +55,7 @@ function inicializarChat() {
     ],
     generationConfig: {
       //define o máximo de tokens que o sistema irá utilizar
-      maxOutputTokens: 1000,
+      maxOutputTokens: 10000,
     },
   });
 }
