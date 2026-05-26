b56eacad043c51fdb70cc0a94acb89e215845fdf
Ian-Alemida <iangtaonline1@gmail.com>
2026-05-26 10:41:02 -0300

    fix: refactor Form component to manage email submission state and improve user feedback

--- src/app/Components/Footer/Infos/Form/Form.jsx
+++ src/app/Components/Footer/Infos/Form/Form.jsx
@@ -1,17 +1,39 @@
+'use client'
+import { useState } from 'react'
 import './form.css'
 
 function Form () {
+    const [email, setEmail] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (email) setSubmitted(true);
+    }
+
     return (
         <div className='Content-Infos-form'>
-            <form action='#email-contato'>
-				<fieldset>
-					<legend className='Content-Infos-form-div-legend' htmlFor="email-contato">Entre em contato:</legend>
-					<div className='Content-Infos-form-div'> 
-						<input className='Content-Infos-form-div-input' type="email" name="email-contato" id="email-contato"></input>
-						<button className='Content-Infos-form-div-button' type="submit">Enviar</button>
-					</div>
-				</fieldset>
-			</form>
+            {submitted ? (
+                <p className='Content-Infos-form-div-legend'>Email recebido! Entrarei em contato em breve.</p>
+            ) : (
+                <form onSubmit={handleSubmit}>
+                    <fieldset>
+                        <legend className='Content-Infos-form-div-legend'>Entre em contato:</legend>
+                        <div className='Content-Infos-form-div'>
+                            <input
+                                className='Content-Infos-form-div-input'
+                                type="email"
+                                name="email-contato"
+                                id="email-contato"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                            <button className='Content-Infos-form-div-button' type="submit">Enviar</button>
+                        </div>
+                    </fieldset>
+                </form>
+            )}
         </div>
     )
 }
